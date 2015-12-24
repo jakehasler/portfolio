bf72bd633c0a827d0e06888f322614252f3d3a9b
Jake Hasler <jakehasler@gmail.com>
2015-12-23 23:57:04 -0700

    Working contact form with Node Mailer!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* Send an Email */
+router.post('/send', function(req, res, next) {
+ 	
+ 	// create reusable transporter object using SMTP transport
+	var transporter = nodemailer.createTransport({
+	    service: 'Gmail',
+	    auth: {
+	        user: 'motoslugger@gmail.com',
+	        pass: '101493jwh'
+	    }
+	});
+
+	// NB! No need to recreate the transporter object. You can use
+	// the same transporter object for all e-mails
+
+	var mailOptions = req.body;
+	console.log(req.body);
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        return console.log(error);
+	    }
+	    console.log('Message sent: ' + info.response);
+
+	});
+
+	res.end("Message Sent");
+});
+
 
 module.exports = router;
